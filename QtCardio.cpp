3eda7ea639a590a5c46a5abd66edae19570dd604
Nicholas LeBoeuf <nicholas.leboeuf@snhu.edu>
2020-05-14 15:10:54 -0400

    cardio workout window sort of complete. just needs a timer

--- QtCardio.cpp
+++ QtCardio.cpp
@@ -5,17 +5,43 @@
 #include "ui_QtCardio.h"
 #include "QtWorkoutSelectionWindow.h"
 
-QtCardio :: QtCardio(QDialog *parent) {
+QtCardio :: QtCardio(QDialog *parent):
+
+        cardioWorkoutOutput { "" }
+
+{
     setupUi(this);
 
-    /*QObject::connect(okCancelButton, SIGNAL(accepted()), this, SLOT(okCancelButtonClickedHandler()));*/
+    QObject::connect(cardioPushButton, SIGNAL(clicked()), this, SLOT(cardioPushButtonClickedHandler()));
+
 }
 void QtCardio::printStringRep() {
     return;
 }
 void QtCardio::updateUI() {
-
+    cardioWorkoutOutputUI->setText(QString::fromStdString(cardioWorkoutOutput));
 }
-void QtCardio::okCancelButtonClickedHandler() {
+void QtCardio::cardioPushButtonClickedHandler() {
     printf("Inside cardioPushButtonClickedHandler()\n");
+
+    if (cardioWorkoutOutput == "") {
+        cardioWorkoutOutput = "45 seconds of High Knees";
+    }
+    else if (cardioWorkoutOutput == "45 seconds of High Knees") {
+        cardioWorkoutOutput = "45 seconds of Jumping Jacks";
+    }
+    else if (cardioWorkoutOutput == "45 seconds of Jumping Jacks") {
+        cardioWorkoutOutput = "45 seconds of Burpees";
+    }
+    else if (cardioWorkoutOutput == "45 seconds of Burpees") {
+        cardioWorkoutOutput = "45 seconds of Running up Stairs";
+    }
+    else if (cardioWorkoutOutput == "45 seconds of Running up Stairs") {
+        cardioWorkoutOutput = "Go for a run! However long you want!";
+    }
+    else if (cardioWorkoutOutput == "Go for a run! However long you want!") {
+        cardioWorkoutOutput = "Cardio workout complete! Great job!";
+    }
+
+    updateUI();
 }
